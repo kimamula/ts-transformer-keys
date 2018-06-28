cac2d38894099c5f0630e8eda92714132f9c45f1
kimamula <kenji.imamula@gmail.com>
2018-06-29 01:47:46 +0900

    add example usages

---
+++ examples/ttypescript/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ts_transformer_keys_1 = require("ts-transformer-keys");
+[];
+var fooKeys = ["foo"];
+console.log(fooKeys[0]);
+["foo", "bar"][1];
+var fooBarBazKeys = ["bar"];
+fooBarBazKeys.forEach(function (key) { return console.log(key); });
