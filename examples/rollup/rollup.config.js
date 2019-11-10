541096a096b5b6dc5bbef037112eca7ed073d94f
kimamula <kenji.imamula@gmail.com>
2019-11-10 17:54:25 +0900

    update examples

--- examples/rollup/rollup.config.js
+++ examples/rollup/rollup.config.js
@@ -1,3 +1,4 @@
+import resolve from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
 import keysTransformer from 'ts-transformer-keys/transformer';
 
@@ -8,6 +9,7 @@ export default {
     format: 'iife'
   },
   plugins: [
+    resolve(),
     typescript({ transformers: [service => ({
       before: [ keysTransformer(service.getProgram()) ],
       after: []
