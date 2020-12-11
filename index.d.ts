1fd864180a795374f17e20805d490dcd2b7a1ed9
Fabian Wassenhoven <fabian.wassenhoven@vomatec.de>
2020-12-11 15:02:47 +0100

    Corrected Typings

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,3 @@
+interface IDictionary<T> {[key: string]: T;}
 export function keys<T extends object>(): Array<keyof T>;
-export function classMembers<T extends object>(): Array<keyof T>;
+export function classMembers<T extends object>(): IDictionary<string>;
